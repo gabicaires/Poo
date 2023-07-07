a9c3b2a2a2739e35b57aed8a82113d96244b73d7
Gabriela Caires <99662184+gabicaires@users.noreply.github.com>
2023-07-06 21:59:37 -0300

    Add files via upload

---
+++ Atividade 3 - Ex 2/src/atividade/pkg3/ex/pkg2/Veiculo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atividade.pkg3.ex.pkg2;
+
+/**
+ *
+ * @author caire
+ */
+public class Veiculo {
+    
+}
